fc275c1dffc8d44d40c6b4ead90c81d1aefdc5dd
Nodir <nodirhikmatov6gmail.com>
2025-04-15 01:51:06 +0900

    fix: refactor pages components

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -4,12 +4,13 @@ import { NextPage } from "next";
 import { useState } from "react";
 import { Stack } from "@mui/material";
 
-const Community: NextPage = () => {
+const AgentList: NextPage = () => {
+  const [title, setTitle] = useState<string>("hello");
   return (
     <div style={{ margin: "20px 0" }}>
-      <Stack className="container">Community</Stack>
+      <Stack className="container">Agent</Stack>
     </div>
   );
 };
 
-export default withLayoutBasic(Community);
+export default withLayoutBasic(AgentList);
--- pages/cs/index.tsx
+++ pages/cs/index.tsx
@@ -4,12 +4,12 @@ import { NextPage } from "next";
 import { useState } from "react";
 import { Stack } from "@mui/material";
 
-const Community: NextPage = () => {
+const CS: NextPage = () => {
   return (
     <div style={{ margin: "20px 0" }}>
-      <Stack className="container">Community</Stack>
+      <Stack className="container">CS</Stack>
     </div>
   );
 };
 
-export default withLayoutBasic(Community);
+export default withLayoutBasic(CS);
